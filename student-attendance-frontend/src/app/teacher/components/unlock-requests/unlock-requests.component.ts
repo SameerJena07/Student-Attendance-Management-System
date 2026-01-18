cb2bf033b3c61901c9395f99d81ddf55f885db8d
Arpit Khatua <arpitkhatua73@gmail.com>
2026-01-18 14:00:45 +0530

    Add unlock  requests.ts

---
+++ student-attendance-frontend/src/app/teacher/components/unlock-requests/unlock-requests.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TeacherService } from '../../../core/services/teacher.service';
+
+@Component({
+  selector: 'app-unlock-requests',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './unlock-requests.component.html',
+})
+export class UnlockRequestsComponent implements OnInit {
+  requests: any[] = [];
+  isLoading = true;
+
+  constructor(private teacherService: TeacherService) {}
+
+  ngOnInit(): void {
+    this.loadRequests();
+  }
+
+  loadRequests() {
+    this.isLoading = true;
+    this.teacherService.getUnlockRequests().subscribe({
+      next: (data) => {
+        console.log('Unlock Requests:', data); // Debugging
+        this.requests = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.isLoading = false;
+      },
+    });
+  }
+  getStatusClass(status: string): string {
+    switch (status?.toUpperCase()) {
+      case 'PENDING':
+        return 'status-pending';
+      case 'APPROVED':
+        return 'status-approved';
+      case 'REJECTED':
+        return 'status-rejected';
+      default:
+        return 'status-pending';
+    }
+  }
+}
